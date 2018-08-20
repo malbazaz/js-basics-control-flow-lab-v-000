831980496e02c7c8b2414edc2d319bb176d9298a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 20:47:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // Write your code in this file!
-let scuberGreetingForFeet(someValue);
-
+function scuberGreetingForFeet(somValue){
+let someValue;
 if (someValue <= 400){
   "This is on me!"
 } else if (scuberGreetingForFeet() >2000) {
   "I will gladly take your thirty bucks."
 } else if (scuberGreetingForFeet() >2500){
   "No can do."
+}
 }
