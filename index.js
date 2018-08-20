8a8649927b9e6ef0ccb74b2fc8c325301617ae2f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 20:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ if (scuberGreetingForFeet <= 400){
   "This is on me!"
 } else if (scuberGreetingForFeet>2000) {
   "I will gladly take your thirty bucks."
+} else if (scuberGreetingForFeet>2500){
+  "No can do."
 }
