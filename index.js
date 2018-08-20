e0525e4befa175b2c893d0c100c6d70e70d4b839
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 21:00:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue){
+let hello;
+if (someValue <= 400){
+hello = "This one is on me!"
+} else if (someValue >2000 && someValue<=2500) {
+hello = "I will gladly take your thirty bucks."
+} else if (someValue >2500){
+  hello = "No can do."
+}
+return hello;
+}
+
+function ternaryCheckCity(value){const msg = value==="NYC"?"Ok, sounds good.":"No go.";
+return msg;
+}
+
+function switchOnCharmFromTip(tip){
+let hi;
+switch(tip){
+case 'generous':
+hi = "Thank you so much."
+break;
+  case 'not as generous':
+hi = "Thank you."
+break;
+default:
+hi = "Bye."
+}
+return hi;
+}
