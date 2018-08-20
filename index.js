2c2b9d6f6a3755396b8d76a8327a0e74f638992e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 20:50:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(someValue){
 let hello;
 if (someValue <= 400){
 hello = "This one is on me!"
-} else if (someValue >2000) {
+} else if (someValue >2000 && someValue<=2500) {
 hello = "I will gladly take your thirty bucks."
 } else if (someValue >2500){
   hello = "No can do."
