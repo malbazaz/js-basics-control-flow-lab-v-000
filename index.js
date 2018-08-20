015bd4d87b3fc40788f0a4f13d82c5c1679eaaf3
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 20:45:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
-let scuberGreetingForFeet;
+let scuberGreetingForFeet();
 
-if (scuberGreetingForFeet <= 400){
+if (scuberGreetingForFeet() <= 400){
   "This is on me!"
-} else if (scuberGreetingForFeet>2000) {
+} else if (scuberGreetingForFeet() >2000) {
   "I will gladly take your thirty bucks."
-} else if (scuberGreetingForFeet>2500){
+} else if (scuberGreetingForFeet() >2500){
   "No can do."
 }
