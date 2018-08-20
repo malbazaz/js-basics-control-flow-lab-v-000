c8642ef4ec10ef123717f1898ed7e0ad62637622
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 20:44:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let scuberGreetingForFeet;
+
+if (scuberGreetingForFeet <= 400){
+  "This is on me!"
+} else if (scuberGreetingForFeet) {
+  "I will gladly take your thirty bucks."
+}
